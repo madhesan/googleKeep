eeea7130f9b5b3cba3cda0afc58692e72cc31589
madhesan <madhesang22@gmail.com>
2022-09-30 11:16:27 +0530

    next

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
+const { json } = require("express");
 
 // database connection
 connection();
@@ -12,7 +13,9 @@ connection();
 // middlewares
 app.use(express.json());
 app.use(cors());
-
+app.get("/",(req,res)=>{
+    res.json("server running");
+})
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
